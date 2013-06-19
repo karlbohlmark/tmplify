806d4f8dcccebf2e8bc3c3ccfec487dff086469e
Karl Böhlmark <karl.bohlmark@edgeware.tv>
2013-06-19 19:13:29 +0200

    a little if

--- index.js
+++ index.js
@@ -30,12 +30,35 @@ function toTemplateDOM(dom) {
 	visitTemplateRoot(dom.shift(), tmpl);
 }
 
+function traverseDepthFirst (tree, fn) {
+	tree.children = tree.children && tree.children.map(function (child) {
+		return traverseDepthFirst(child, fn)
+	})
+	return fn(tree)
+}
+
+function ifTransform (node) {
+	if (node.attribs && typeof node.attribs['if'] != 'undefined') {
+		return {
+			type: 'if',
+			test: node.attribs['if'],
+			consequent: node
+		}
+	} else {
+		console.log('ATTRS', node.attribs)
+	}
+	return node
+}
+
 function visitTemplateRoot (node, parentNode) {
 	if (node.type != 'tag') {
 		throw new Error('Top level should be a tag');
 	}
 
-	var body = visitTag(node);
+	node = traverseDepthFirst(node, ifTransform)
+	//traverseDepthFirst(node, eachTransform)
+
+	var body = visit(node);
 	
 	var bufferInit = b.variableDeclaration('var', [
 		b.variableDeclarator(
@@ -44,25 +67,22 @@ function visitTemplateRoot (node, parentNode) {
 		)
 	])
 
-	var entry = body[body.length-1].id
+	var entry = body[body.length-1]
+
+	//console.log('ENTRY', body[body.length-1])
+	var fnDeclarations = functionDeclarations(body);
 
 	var program = b.program(
 			concat(
 				bufferInit,
-				body,
+				fnDeclarations,
 				singleExport(
 					b.functionExpression(
 						b.identifier("main"),
 						[b.identifier("model")],
 						b.blockStatement([
 							declareEmptyArray('buffer'),
-							b.expressionStatement(
-								b.callExpression(
-									entry,
-									[b.identifier('model'), b.identifier('buffer')]
-								)
-							),
-
+							entryStatement(entry),
 							b.returnStatement(
 								b.callExpression(
 									b.memberExpression(
@@ -83,6 +103,22 @@ function visitTemplateRoot (node, parentNode) {
 	console.log(escodegen.generate(program));
 }
 
+
+function entryStatement(entry) {
+	if (entry.type == 'FunctionDeclaration') {
+		return b.expressionStatement(
+			b.callExpression(
+				b.identifier(entry.id.name),
+				[b.identifier('model'), b.identifier('buffer')]
+			)
+		)	
+	}
+
+	if (entry.type == 'IfStatement') {
+		return entry
+	}	
+}
+
 function visit(node) {
 	switch (node.type) {
 		case 'tag':
@@ -91,13 +127,46 @@ function visit(node) {
 		case 'text':
 			return visitText(node)
 			break;
+		case 'if':
+			return visitIf(node)
 		default:
 			throw new Error('unknown type', node)
 	}
 }
 
+function visitIf(node) {
+	var cons = visit(node.consequent)
+	var consBlock = cons[cons.length-1]
+
+	console.log('BODY')
+	console.log(consBlock)
+
+	var body = consBlock.body.body
+	consBlock.body.body = [b.ifStatement(
+			b.identifier(node.test),
+			b.blockStatement(body)
+		)
+	]
+
+	var declarations = functionDeclarations(cons)
+
+	return declarations.concat([
+		consBlock
+	])
+}
+
+function functionDeclarations(arr) {
+	return arr.filter(function (item) {
+		return item.type == 'FunctionDeclaration'
+	})
+}
+
 function visitTag(tag, parentJsNode) {
 	log.debug(tag)
+
+	if (!tag.name) {
+		throw new Error('Tags should have a name. Tag: ' + Object.keys(tag).join(''))
+	}
 
 	var children = tag.children && tag.children.map(visit) || []
 	var topLevel = children.map(last)
